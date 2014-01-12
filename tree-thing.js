d5e8addb8d8729f403a617788114ef10a7f09b4b
Mattias Petter Johansson <mpj@spotify.com>
2014-01-12 17:53:32 +0100

    partial implementation of stream

--- tree-thing.js
+++ tree-thing.js
@@ -2,14 +2,20 @@ var chai = require('chai')
 var expect = chai.expect
 chai.should()
 var Q = require('q')
+Q.longStackSupport = true;
 var forEach = require('mout/array/forEach')
+var combine = require('mout/array/combine')
 var deepClone = require('mout/lang/deepClone')
 
+// Not using mouts deepEquals because we need array support.
+var deepEquals = require('deep-equal')
 
-// TODO: Stream
+
+// TODO Stream on remove
 var TreeThing = {
   ensureInit: function() {
-    this._changes = this._changes || []
+    this._changes  = this._changes || []
+    this._watchers = this._watchers || []
   },
   _generateTreeFromChanges: function(until) {
     var root = {}
@@ -33,14 +39,56 @@ var TreeThing = {
     })
     return root
   },
+  _notifyAboutChanges: function(path, before, after) {
+    var me = this
+    var partialPathParts = []
+    return Q.all(
+      // Generate all paths that are dependent on
+      // this change.
+      path.split('/').map(function(part) {
+        partialPathParts.push(part)
+        return partialPathParts.join('/')
+      }).map(function(notifyPath) {
+        if (!me._watchers[notifyPath] ||
+             me._watchers[notifyPath].length === 0) {
+          // Nothing listening on this path, don't bother
+          // doing any work.
+          return Q(true);
+        }
+        // Only notify the watcher if the change actually caused
+        // the result to change.
+        return Q.spread([
+          me.snapshot(notifyPath, before),
+          me.snapshot(notifyPath, after)
+        ], function(beforeValue, afterValue) {
+          if (!deepEquals(beforeValue, afterValue)) {
+            me._watchers[notifyPath].forEach(function(watcher) {
+              watcher(afterValue)
+            })
+          }
+
+        })
+      })
+    )
+  },
   put: function(path, data) {
-    this.ensureInit()
-    this._changes.push({
+    var me = this;
+    me.ensureInit()
+
+    var beforeChangeSequence = me._changes.length - 1
+
+    me._changes.push({
       type: 'put',
       path: path,
       data: data
-    })
-    return Q(this._changes.length-1)
+    });
+
+    var afterChangeSequence = me._changes.length - 1
+
+    return me._notifyAboutChanges(path, beforeChangeSequence, afterChangeSequence)
+      .then(function() {
+        return afterChangeSequence
+      })
   },
   remove: function(path) {
     this.ensureInit()
@@ -51,14 +99,32 @@ var TreeThing = {
     return Q(this._changes.length-1)
   },
   snapshot: function(path, until) {
+    if (until === -1) return Q(null);
     this.ensureInit()
     var root = this._generateTreeFromChanges(until);
     var cursor = root;
-    var pathPaths = path.split('/')
-    pathPaths.forEach(function(part) {
+    var pathParts = path.split('/')
+    forEach(pathParts, function(part) {
+      if (!cursor[part]) {
+        // Path does not exist, just return null
+        cursor = null
+        return false
+      }
       cursor = cursor[part]
     })
     return Q(cursor)
+  },
+  stream: function(path) {
+    var me = this
+    me.ensureInit()
+
+    return {
+      onChange: function(fn) {
+        me._watchers[path] = me._watchers[path] || []
+        me._watchers[path].push(fn)
+        me.snapshot(path).then(fn)
+      }
+    }
   }
 }
 
@@ -114,6 +180,28 @@ describe('When we have a thing', function() {
       return tt.snapshot('animals/dogs/chiuauas', 0)
     }).then(function(chiuauas0) {
       chiuauas0.length.should.equal(1)
+    })
+    .done(done)
+  })
+
+
+  it('should stream changes to watchers', function(done) {
+
+    var dogsStreamed = undefined;
+    tt.stream('animals/dogs').onChange(function(dogs) {
+      dogsStreamed = dogs
+    })
+
+    tt.put('animals/cats', [{ name: 'Mittens' }]).then(function() {
+      expect(dogsStreamed).to.be.null
+    }).then(function() {
+      return tt.put('animals/dogs', [{ name: 'Karo' }])
+    }).then(function() {
+      dogsStreamed[0].name.should.equal('Karo')
+      dogsStreamed = null
+      return tt.put('animals/dogs', [{ name: 'Karo' }]) //same as before
+    }).then(function() {
+      expect(dogsStreamed).to.be.null // was unchanged, therefore not strean
     })
     .done(done)
   })
